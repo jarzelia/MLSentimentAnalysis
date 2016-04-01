6d089a558f596bcd37a1654310e559a0847a68b7
patleh2 <patleh2@gmail.com>
2016-04-01 18:44:02 -0400

    adding python parser and init output

---
+++ dataio/parser.py
@@ -0,0 +1,37 @@
+import os
+import re
+
+dataPos = []
+dataNeg = []
+wordsPos = {}
+wordsNeg = {}
+
+path = "../train/pos/"
+
+for f in os.listdir(path):
+        filePath = os.path.join(path, f)
+        dataPos.append(open(filePath, 'r', encoding='utf-8').read())
+
+
+path2 = "../train/neg/"
+for f in os.listdir(path2):
+        filePath = os.path.join(path2, f)
+        dataNeg.append(open(filePath, 'r', encoding='utf-8').read())
+
+
+# regex needs work or need to trim strings with ','
+# losing about 1/2 of our strings
+for review in dataPos:
+        tokens = re.split('[^a-zA-Z0-9_]', review)
+        
+        for t in tokens:
+                if t in wordsPos:
+                        wordsPos[t] = wordsPos[t] + 1
+                else:
+                        wordsPos[t] = 1
+                        
+fOut = open('pos.dat', 'w', encoding='utf-8')
+
+for k, v in wordsPos.items():
+        fOut.write(k + " " + str(v) + '\n')
+
