4942f1a589551d093e8ac1043af4f194e7045944
patleh2 <patleh2@gmail.com>
2016-04-20 11:43:04 -0400

    aaaaa

--- dataio/parser.py
+++ dataio/parser.py
@@ -1,3 +1,4 @@
+from collections import Counter
 import os
 import re
 
@@ -10,44 +11,54 @@ path = "../train/pos/"
 
 for f in os.listdir(path):
         filePath = os.path.join(path, f)
-        dataPos.append(open(filePath, 'r', encoding='utf-8').read())
-
+        theFile = open(filePath, 'r', encoding='utf-8')
+        dataPos.append(theFile.read().lower())
+        theFile.close()
 
 path2 = "../train/neg/"
 for f in os.listdir(path2):
         filePath = os.path.join(path2, f)
-        dataNeg.append(open(filePath, 'r', encoding='utf-8').read())
+        theFile = open(filePath, 'r', encoding='utf-8')
+        dataNeg.append(theFile.read().lower())
+        theFile.close()
 
-
-# regex needs work or need to trim strings with ','
-# losing about 1/2 of our strings
 for review in dataPos:
-        tokens = re.split('[^a-zA-Z0-9_]', review)
+        tokens = re.split("\s+", review)
+
+        # Remove duplicates
+        tokens = list(set(tokens))     
         
         for t in tokens:
+                # Remove non-alphanumeric characters
+                t = re.sub('\W+', '', t)
                 if t in wordsPos:
                         wordsPos[t] = wordsPos[t] + 1
                 else:
                         wordsPos[t] = 1
-                        
-fOut = open('pos_data', 'w', encoding='utf-8')
 
-for k, v in wordsPos.items():
-        fOut.write(k + " " + str(v) + '\n')
-
+                      
+fOut = open('pos_data.dat', 'w')
+for w in sorted(wordsPos, key = wordsPos.get, reverse=True)[:1000]:
+        fOut.write(str(wordsPos[w]) + " " + w + "\n")
+fOut.close()
 
 
 for review in dataNeg:
-        tokens = re.split('[^a-zA-Z0-9_]', review)
+        tokens = re.split("\s+", review)
+
+        # Remove duplicates list->set->list
+        tokens = list(set(tokens))
         
         for t in tokens:
+                # Remove non-alphanumeric characters
+                t = re.sub('\W+', '', t)
                 if t in wordsNeg:
                         wordsNeg[t] = wordsNeg[t] + 1
                 else:
                         wordsNeg[t] = 1
 
-fOut2 = open('neg_data', 'w', encoding='utf-8')
+fOut2 = open('neg_data.dat', 'w')
+for w in sorted(wordsNeg, key = wordsNeg.get, reverse=True)[:1000]:
+        fOut2.write(str(wordsNeg[w]) + " " + w + "\n")
+fOut2.close()
 
-for k, v in wordsNeg.items():
-        fOut2.write(k + " " + str(v) + '\n')
-        
